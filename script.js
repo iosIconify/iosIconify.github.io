1af085ad58b8affdfdb011be71c5ee72a8d2b67f
Booligoosh <booligoosh@optusnet.com.au>
2017-05-05 15:16:06 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function goToPage(str) {
+  var pages = document.getElementsByClassName('page');
+  for (var i = 0; i < pages.length; i++) {
+    document.getElementById('page-' + pages[i]).style = 'display: none;';
+  }
+  document.getElementById('page-' + str).style = 'display: block;';
+}
