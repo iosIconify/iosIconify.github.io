ebe78290ba73e87c412236a9b2792316df4809bc
Booligoosh <booligoosh@optusnet.com.au>
2017-05-06 17:39:45 +1000

    removed debug alerts

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function goToPage(str) {
-  //alert('hi!');
-  //alert(document.getElementsByClassName('page'));
   var pages = document.getElementsByTagName('page');
   for (var i = 0; i < pages.length; i++) {
     pages[i].style.display = 'none';
-    alert(pages[i].style);
   }
   document.getElementById('page-' + str).style.display = 'block';
 }
